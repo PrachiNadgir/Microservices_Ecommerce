04ef825415e1ae1fa74c931eb37f48835d4c6811
Prachi Nadgir <nadgirprachi@gmail.com>
2026-04-28 18:14:41 +0530

    AuthController.cs

--- AuthService/Controllers/AuthController.cs
+++ AuthService/Controllers/AuthController.cs
@@ -67,7 +67,9 @@ namespace AuthService.Controllers
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Email, user.Email),
-                new Claim("role", user.Role) // ✅ important
+                new Claim("role", user.Role)
+                new Claim(ClaimTypes.Role, user.Role)
+                // ✅ important
             };
 
             var token = new JwtSecurityToken(
